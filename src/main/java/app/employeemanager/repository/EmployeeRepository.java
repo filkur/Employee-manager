3b8d3405873488d5e207590ecc2d54531697c4dc
filkur <filipkuranda@gmail.com>
2021-02-18 10:36:22 +0100

    Add employee repo

--- src/main/java/app/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/app/employeemanager/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package app.employeemanager.repository;
 import app.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
 
+    Optional<Employee> findAllById(int id);
 }
